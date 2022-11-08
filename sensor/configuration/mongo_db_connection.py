daa1c7857ca41ab1d1fd991d9faa8805dabf8824
Atul Kumar <atul.kmr011@gmail.com>
2022-11-08 16:49:56 -0500

    training pipeline

--- sensor/configuration/mongo_db_connection.py
+++ sensor/configuration/mongo_db_connection.py
@@ -1,5 +1,7 @@
 import pymongo
 from sensor.constant.database import DATABASE_NAME
+from sensor.constant.env_variable import MONGODB_URL_KEY
+import os
 import certifi
 ca = certifi.where()
 
@@ -8,7 +10,8 @@ class MongoDBClient:
     def __init__(self, database_name=DATABASE_NAME) -> None:
         try:
             if MongoDBClient.client is None:
-                mongo_db_url = "mongodb+srv://atulk7328:zDc4l6KPKX1X8ofp@cluster0.icaw2ok.mongodb.net/?retryWrites=true&w=majority"
+                mongo_db_url = os.getenv(MONGODB_URL_KEY)
+                #MongoDBClient.client = pymongo.MongoClient(os.environ, tlsCAFile=ca)
                 MongoDBClient.client = pymongo.MongoClient(mongo_db_url, tlsCAFile=ca)
             self.client = MongoDBClient.client
             self.database = self.client[database_name]
---
+++ sensor/constant/env_variable.py
@@ -0,0 +1,4 @@
+MONGODB_URL_KEY = "mongodb+srv://atulk7328:zDc4l6KPKX1X8ofp@cluster0.icaw2ok.mongodb.net/?retryWrites=true&w=majority"
+AWS_ACCESS_KEY_ID_ENV_KEY = "AWS_ACCESS_KEY_ID"
+AWS_SECRET_ACCESS_KEY_ENV_KEY = "AWS_SECRET_ACCESS_KEY"
+REGION_NAME = "us-east-1"
